f5cfb92b8f60d58845b3b808b4a766a19d79bd62
Q <q@mail.ru>
2020-05-30 23:46:55 +0500

    Add show vk forwarded messages id

---
+++ vk_print_fwdmsg_id.js
@@ -0,0 +1,19 @@
+function xForwardedMessagesShowConversationId() {
+    const msgs = document.getElementsByClassName('im-mess');
+    for (let i = 0; i < msgs.length; i++) {
+        const msg = msgs[i];
+        if (!msg.hasAttribute('data-x-id_checked')) {
+            const msgid = msg.getAttribute('data-msgid');
+            const m = msgid.match(/_(\d+)_/);
+            if (m) {
+                convId = m[1];
+                const textBlock = msg.getElementsByClassName('im-mess--text')[0];
+                const html = '<span style="color:var(--blue_400)"> ' + convId + '</span>';
+                textBlock.insertAdjacentHTML('beforeend', html);
+            }
+            msg.setAttribute('data-x-id_checked', '1');
+        }
+    }
+}
+
+xForwardedMessagePrintConversationId();
